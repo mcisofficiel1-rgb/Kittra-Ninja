5ad7b9e732e3e5e5b2d51c5f12a52cd0ab3a8061
mcisofficiel1-rgb <mcis.officiel1@gmail.com>
2026-09-10 22:21:16 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,9 @@
 self.addEventListener('install', e=>{self.skipWaiting()});
-self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+self.addEventListener('activate', e=>{self.clients.claim()});
+self.addEventListener('fetch', e=>{
+  // Ne jamais cacher /api/* et /ping pour que UptimeRobot marche
+  if(e.request.url.includes('/api/') || e.request.url.includes('/ping')){
+    return e.respondWith(fetch(e.request));
+  }
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
