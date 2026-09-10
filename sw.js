3d7931c6f604f232d41bd8b481a14f2d31341b2f
mcisofficiel1-rgb <mcis.officiel1@gmail.com>
2026-09-10 11:52:46 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,2 @@
-```javascript
-// sw.js - Ninja furtif
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => console.log("🔒 KITTRA VAULT NINJA ACTIF"));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
-```
+self.addEventListener('install', e=>{self.skipWaiting()});
+self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
